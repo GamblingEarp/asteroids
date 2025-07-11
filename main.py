d030b2fdf32c94c757f5cb4e611c2decc4bf4658
GamblingEarp <wyattroqs@gmail.com>
2025-07-11 10:34:27 -0700

    containers added

--- main.py
+++ main.py
@@ -9,18 +9,28 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill("black")
+        updatable.update(dt)
 
-        player.draw(screen)
-        player.update(dt)
+        screen.fill("black")
+        for item in drawable:
+            item.draw(screen)
 
         pygame.display.flip()
 
